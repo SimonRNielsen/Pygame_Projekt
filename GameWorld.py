2e798428c1004a82fc7cfb1c3b8e082ff0b6f238
SimonRNielsen <simonrn@gmail.com>
2026-02-07 14:16:18 +0100

    Added assets

--- GameWorld.py
+++ GameWorld.py
@@ -5,9 +5,13 @@ class GameWorld:
     def __init__(self) -> None:
         pygame.init()
 
-        self._screen = pygame.display.set_mode((800,600))
+        self._screen = pygame.display.set_mode((1280,1024))
         self._running = True
         self._clock = pygame.time.Clock()
+
+        self._sprite_image = pygame.image.load("assets\\player.png")
+        self._sprite = pygame.sprite.Sprite()
+        self._sprite.rect = self._sprite_image.get_rect()
 
     def awake(self):
         pass
@@ -21,6 +25,9 @@ class GameWorld:
                 if event.type == pygame.QUIT:
                     self._running = False
             self._screen.fill("cornflowerblue")
+
+            self._screen.blit(self._sprite_image,self._sprite.rect)
+
             pygame.display.flip()
             self._clock.tick(60)
         pygame.quit()
